ac523ad0d29d9a4e794265128edcaf6eb66af4b9
anton <altecsyst@gmail.com>
2021-08-07 23:25:38 +0300

    repo initialization

---
+++ RedisExamples.AwsElasticacheSamples/RedisExamples.AwsElasticacheSamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RedisExamples.AwsElasticacheSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
