0097f258fff1eae1b55f7ef435affb32e02405f8
anton <altecsyst@gmail.com>
2021-08-08 00:23:31 +0300

    added redis processor

---
+++ RedisExamples.AwsElasticacheSamples/RedisExamples.AwsElasticacheSamples/RedisProcessor.cs
@@ -0,0 +1,29 @@
+﻿using StackExchange.Redis;
+using System;
+using System.IO;
+
+namespace RedisExamples.AwsElasticacheSamples
+{
+    class RedisProcessor
+    {
+        private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            var log = new StringWriter();
+            ConfigurationOptions option = new ConfigurationOptions
+            {
+                AbortOnConnectFail = false
+                //ConnectTimeout = 20000
+            };
+            option.EndPoints.Add("vehiclehub-cbb-redis.flijwf.ng.0001.cac1.cache.amazonaws.com:6379");
+            return ConnectionMultiplexer.Connect(option, log);
+        });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+    }
+}
